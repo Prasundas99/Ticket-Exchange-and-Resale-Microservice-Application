1e5d554330dbaf44bb72c8e9b6b5e85bb0b1e98e
Prasundas99 <prasundas115@gmail.com>
2021-09-20 00:32:33 +0530

    Error handelers

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,14 +1,19 @@
 import express from 'express'
 import { json } from 'body-parser'
 
+//routes
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin'
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
 
+//middleware
+import {errorHandler} from './middlewares/error-handler'
+
 
 const app = express()
 app.use(json());
+app.use(errorHandler);
 
 app.use(currentUserRouter);
 app.use(signinRouter)
