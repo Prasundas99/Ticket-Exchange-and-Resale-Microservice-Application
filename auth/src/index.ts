561b40d2e48b4d3362e1bfbda5fb2d48110febfc
Prasundas99 <prasundas115@gmail.com>
2021-09-21 01:29:47 +0530

    Not found error handeled

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,18 +5,27 @@ import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
+//error handeler
 import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
+//middlewares
 app.use(json());
+app.all('*', () => {
+  throw new NotFoundError();
+});
+app.use(errorHandler);
 
+
+//routes
 app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
 
-app.use(errorHandler);
+
 
 app.listen(3000, () => {
-  console.log('Listening on port 3000!!!!!!!!');
+  console.log('Listening on port 3000..');
 });
