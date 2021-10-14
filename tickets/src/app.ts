e3dab7bf0086fd07ed00e5fddab9a44b5fe4397a
Prasundas99 <prasundas115@gmail.com>
2021-10-14 23:04:05 +0530

    Docker build

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,13 +2,7 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
-
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
+import { NotFoundError, errorHandler } from '@sgtickets/common';
 
 const app = express();
 app.set('trust proxy', true);
@@ -20,10 +14,6 @@ app.use(
   })
 );
 
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
