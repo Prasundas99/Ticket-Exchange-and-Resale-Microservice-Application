cbfb54f59e8a6de58e4d05ff0e2fc4c2b48db960
Prasundas99 <prasundas115@gmail.com>
2021-10-14 22:50:28 +0530

    Project setup for ticketing services

---
+++ tickets/src/index.ts
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+
+import { app } from './app';
+
+
+//another file is created to connect db for prod code to prevent conflict of prod and test server in test we are using mongo server and prob we are using mongodb
+
+const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY must be defined');
+  }
+
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    });
+    console.log('Connected to MongoDb');
+  } catch (err) {
+    console.error(err);
+  }
+
+  app.listen(3000, () => {
+    console.log('Listening on port 3000!!!!!!!!');
+  });
+};
+
+start();
